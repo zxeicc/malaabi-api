37937b550ffb5876cbf88aa0ee3927a27f8b7b16
zxeicc <mohamedsallehamin@gmail.com>
2026-06-10 21:24:06 +0100

    fix: correct mailer path

--- config/mailer.js
+++ config/mailer.js
@@ -12,6 +12,8 @@ const transporter = nodemailer.createTransport({
 transporter.verify((err, success) => {
     if (err) console.log('Email error:', err.message);
     else console.log('Email ready ✅');
-});
+    
+});console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
 
 module.exports = transporter;
