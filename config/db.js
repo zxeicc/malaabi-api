e9de6f0c1103872fca8dc58ce8fb42595bd87785
zxeicc <mohamedsallehamin@gmail.com>
2026-06-10 19:22:48 +0100

    fix: db config and dynamic PORT

--- config/db.js
+++ config/db.js
@@ -1,6 +1,23 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+db.getConnection()
+  .then(() => console.log("✅ Database connected successfully"))
+  .catch((err) => console.error("❌ DB connection error:", err));
+
+module.exports = db;/*require("dotenv").config();
+const mysql = require("mysql2/promise");
+
 // استخدم MYSQL_URL أو DATABASE_URL
 const db = mysql.createPool(process.env.MYSQL_URL || process.env.DATABASE_URL);
 
