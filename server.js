8a89883b9c10e94e5d775b523970bddc09045946
zxeicc <mohamedsallehamin@gmail.com>
2026-06-11 02:51:55 +0100

    debug: check mailer load

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const role = require("./middleware/role");
 require("dotenv").config();
 require('./config/mailer');
+console.log("mailer required ✅");
 const cors = require("cors");
 const auth = require("./middleware/auth");
 const bookingsRoutes = require('./routes/bookingsRoutes');
