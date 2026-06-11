8da98e3035554db2c7061a755754ab4693505e56
zxeicc <mohamedsallehamin@gmail.com>
2026-06-11 04:18:52 +0100

    fix: add email to updateBookingStatus

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -70,7 +70,8 @@ exports.updateBookingStatus = async (req, res) => {
       const date = String(b.date).split("T")[0]
       console.log("Sending email to:", b.player_email, "status:", status)
       if (status === "confirmed") {
-        const date = String(b.date).split("T")[0]
+        
+    // const date = String(b.date).split("T")[0]
         await sendBookingEmail(b.player_email, "✅ تم قبول حجزك في ملاعبي", confirmedEmail(b.player_name, b.field_name, date, b.time_slot))
       } else if (status === "rejected") {
         await sendBookingEmail(b.player_email, "❌ تم رفض حجزك في ملاعبي", rejectedEmail(b.player_name, b.field_name, date, b.time_slot))
